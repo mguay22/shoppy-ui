d0ae35b1a7ff400d56485e87af8f26496d9137ca
Michael Guay <michaelguay@Michaels-Mac-Studio.local>
2024-02-26 21:00:26 -0500

    Material UI

---
+++ app/dark.theme.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import { createTheme } from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default darkTheme;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Container, CssBaseline, ThemeProvider } from "@mui/material";
+import darkTheme from "./dark.theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Container>{children}</Container>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
