98ea2fdbda13e50a7bc4ef40b491bed82283bc24
Michael Guay <michaelguay@Michaels-Mac-Studio.local>
2024-04-08 22:47:54 -0400

    Cleanup implementation

--- app/auth/signup/create-user.ts
+++ app/auth/signup/create-user.ts
@@ -4,9 +4,9 @@ import { post } from "@/app/util/fetch";
 import { redirect } from "next/navigation";
 
 export default async function createUser(_prevState: any, formData: FormData) {
-  const parsedRes = await post("users", formData);
-  if (parsedRes) {
-    return parsedRes;
+  const { error } = await post("users", formData);
+  if (error) {
+    return { error };
   }
   redirect("/");
 }
