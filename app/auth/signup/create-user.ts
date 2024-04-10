e845f834853f39109b980621bee642d36d0e510f
Michael Guay <michaelguay@Michaels-Mac-Studio.local>
2024-04-09 20:36:33 -0400

    Missing type

--- app/auth/signup/create-user.ts
+++ app/auth/signup/create-user.ts
@@ -1,9 +1,13 @@
 "use server";
 
-import { post } from "@/app/util/fetch";
 import { redirect } from "next/navigation";
+import { FormError } from "@/app/common/form-error.interface";
+import { post } from "@/app/util/fetch";
 
-export default async function createUser(_prevState: any, formData: FormData) {
+export default async function createUser(
+  _prevState: FormError,
+  formData: FormData
+) {
   const { error } = await post("users", formData);
   if (error) {
     return { error };
